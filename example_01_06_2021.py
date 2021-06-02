3c4d460d220beeb7d70c2bb10fbc61cf2e8bac24
Erick Palacios Moreno <>
2021-06-02 01:41:12 +0000

    docstring of middle_point function

--- example_01_06_2021.py
+++ example_01_06_2021.py
@@ -1,10 +1,19 @@
-def middle_point(p1, p2): #parámetros p1, p2
-    return (p1+p2)/2
+# Librerias.
+import numpy as np 
 
+# Función para calculo de punto medio.
+def middle_point(p1, p2):
+   #parámetros p1, p2
+    return (p1+p2)/2 
 
+# Parametro 1
 p1 = 7 
+# Parametro 2
 p2 = 3.1
 
 m = middle_point(p1, p2)  
+#n output with the result
 
+
+n = middle_point(7, np.pi())
 print(m)
