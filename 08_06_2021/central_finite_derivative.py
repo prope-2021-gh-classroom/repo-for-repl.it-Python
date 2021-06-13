b4da29bea9ac3177b7deafda49b4c6a5b7436291
Erick Palacios Moreno <palmoreck@gmail.com>
2021-06-13 14:03:02 -0500

    docstring for funcs

--- 08_06_2021/central_finite_derivative.py
+++ 08_06_2021/central_finite_derivative.py
@@ -3,14 +3,28 @@ import numpy as np
 
 def approx_first_derivative(fun, x, h):
     """
-    Esta función regresa la primera derivada de la función 'fun', tomando como parámetros x (el punto dónde se evalua) y h (el tamaño del paso).
+    Numerical differentiation by finite differences. Uses central point formula
+    to approximate first derivative of function.
+    Args:
+        f (function): function definition.
+        x (float): point where first derivative will be approximated
+        h (float): step size for central differences. Tipically less than 1
+    Returns:
+        df (float): approximation to first_derivative.
     """
     df = (fun(x+h) - fun(x-h))/(2*h)
     return df
 
 def approx_second_derivative(fun, x, h):
     """
-    Esta función regresa la segunda derivada de la función 'fun', tomando como parámetros x, h
+    Numerical differentiation by finite differences. Uses central point formula
+    to approximate second derivative of function.
+    Args:
+        f (function): function definition.
+        x (float): point where second derivative will be approximated
+        h (float): step size for central differences. Tipically less than 1
+    Returns:
+        ddf (float): approximation to second_derivative.
     """ 
     ddf =(fun(x+h) - 2.0*fun(x) + fun(x-h))/h**2
     return ddf
